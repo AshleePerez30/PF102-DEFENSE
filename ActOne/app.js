075b37842073db466b0407d1ecbc95f995ff25d8
fapaholic <jaspervincem.99@gmail.com>
2024-03-22 10:36:38 +0800

    my new

---
+++ ActOne/app.js
@@ -0,0 +1,21 @@
+import { leftDiv } from "./info.js"
+import { rightDiv } from "./img.js"
+
+
+let container = document.getElementById("container")
+const data = {
+    title: "Aoyama Yuga",
+    title2: "I Can Not Stop Twinkling.",
+    para: "Yuga Aoyama, also known as the Shining Hero: Can't Stop Twinkling, is a recurring character in My Hero Academia. He is a student in Class 1-A at U.A. High School, training to become a Pro Hero. Later on, it was revealed that he was the U.A.",
+    para2: "Quirk: Navel Laser; Likes: Himself",
+    button: "ABOUT",
+    button2: "HOBBIES",
+    button3: "AFFILIATION",
+    button4: "CONTACT",
+    img: "img/cant.jpg"
+}
+
+const {title, title2, para, para2, button, button2, button3, button4, img} = data
+
+container.append(leftDiv(title,title2,para,para2,button,button2,button3,button4))
+container.append(rightDiv(img))
---
+++ ActTwo/js/app.js
@@ -0,0 +1,41 @@
+import {image} from "./img.js"
+import {info} from "./info.js"
+import {cards} from "./card.js"
+
+const myObj1 = {
+    title : "My portfolio",
+    disc : "Hi! My name is Ashlee Jaucian J. Perez, 19 years old, from purok 6, Brgy.25, Gingoog City. I am currently a 2nd year BSIT Student from GCCi.",
+    btn : "About me",
+    myimg : "myImg.png"
+}
+
+const myObj2 = {
+    comp : "Project Completed",
+
+    img1 : "img1.PNG",
+    dis1 : "This is our final project output for the subject Robotics/IICT. We put efforts and time in making this final project and we made it sucessfully",
+    btn1 : "Learn More",
+
+    img2 : "img2.PNG",
+    dis2 : "This is our midterm project output for the subject Robotics/IICT, mini-model of GCCi campus, using Arduino Uno and series leds",
+    btn2 : "Learn More",
+
+    img3 : "img3.PNG",
+    dis3 : "Our final performance project for the subject Philippine History, it was all about First Voyage around the World, and it was hard but fun",
+    btn3 : "Learn More"
+}
+
+let sectionOne = document.getElementById("sectionOne")
+let sectionTwo = document.getElementById("sectionTwo")
+
+const {title,disc,btn,myimg} = myObj1;
+
+const {comp,img1,dis1,btn1,img2,dis2,btn2,img3,dis3,btn3} = myObj2;
+
+sectionOne.append(info(title,disc,btn))
+sectionOne.append(image(myimg))  
+
+sectionTwo.append(cards(comp,img1,dis1,btn1,img2,dis2,btn2,img3,dis3,btn3))
+
+
+
