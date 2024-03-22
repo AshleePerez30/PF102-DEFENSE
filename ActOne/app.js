c80f987bf6a781f1736799bf3eafb61958737a35
AshleePerez30 <ashleejaucianperez@gmail.com>
2024-03-22 17:03:53 +0800

    PF102-DEFENSE

--- ActOne/app.js
+++ ActOne/app.js
@@ -9,9 +9,9 @@ const data = {
     para: "Yuga Aoyama, also known as the Shining Hero: Can't Stop Twinkling, is a recurring character in My Hero Academia. He is a student in Class 1-A at U.A. High School, training to become a Pro Hero. Later on, it was revealed that he was the U.A.",
     para2: "Quirk: Navel Laser; Likes: Himself",
     button: "ABOUT",
-    button2: "HOBBIES",
-    button3: "AFFILIATION",
-    button4: "CONTACT",
+    button2: "PERSONALITY",
+    button3: "ABILITY",
+    button4: "EQUIPMENT",
     img: "img/cant.jpg"
 }
 
--- ActOne/info.js
+++ ActOne/info.js
@@ -5,10 +5,10 @@ const leftDiv = (t1, t2, p1, p2, b1, b2, b3, b4) => {
                      <p>${p1}</p>
                      <h1>${t2}</h1>
                      <p>${p2}</p>
-                     <button>${b1}</button>
-                     <button>${b2}</button>
-                     <button>${b3}</button>
-                     <button>${b4}</button>
+                     <button><a href="https://myheroacademia.fandom.com/wiki/Yuga_Aoyama"></a>${b1}</button>
+                     <button><a href="https://myheroacademia.fandom.com/wiki/Yuga_Aoyama#Personality"></a>${b2}</button>
+                     <button><a href="https://myheroacademia.fandom.com/wiki/Yuga_Aoyama#Abilities"></a>${b3}</button>
+                     <button><a href="https://myheroacademia.fandom.com/wiki/Yuga_Aoyama#Equipment"></a>${b4}</button>
     `
     return div
 }
